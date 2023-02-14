a5e2d89080d766cb32aad309a1d632cafe9bac0d
brn091 <brn091dev@gmail.com>
2023-02-14 17:26:35 -0500

    game selection

--- src/components/PokemonOptions.vue
+++ src/components/PokemonOptions.vue
@@ -1,16 +1,29 @@
 <template>
   <div class="options-container">
     <ul>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-      <li>4</li>
+      <li
+        v-for="pokemon in pokemons"
+        :key="pokemon.id"
+        @click="$emit('selectPokemon', pokemon.id, pokemon.name)"
+      >
+        {{ pokemon.name }}
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: {
+    pokemons: {
+      type: Array,
+      required: true,
+    },
+  },
+  // emits() {
+  //   'select-pokemon'
+  // }
+};
 </script>
 
 <style>
--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -1,23 +1,64 @@
 <template>
-  <h1>¿Quien es este Pokemon?</h1>
-  <PokemonImage v-bind:pokemonId="1" :showPokemon="true"/>
-  <PokemonOptions />
+  <h1 v-if="!pokemon">Esperanding...</h1>
+  <div v-else>
+    <h1>¿Quién es este Pokemon?</h1>
+    <PokemonImage v-bind:pokemonId="pokemon.id" :showPokemon="showPokemon" />
+    <PokemonOptions :pokemons="pokemonArr" @select-pokemon="checkAnswer" />
+
+    <template v-if="showAnswer">
+      <h2 class="fade-in">{{ msg }}</h2>
+      <button @click="newGame">Nuevo juego</button>
+    </template>
+  </div>
 </template>
 
 <script>
 import PokemonImage from "@/components/PokemonImage";
 import PokemonOptions from "@/components/PokemonOptions";
-import getPokemonsOptions from '@/helpers/getpokemonOptions'
+import getPokemonsOptions from "@/helpers/getpokemonOptions";
 export default {
   components: {
     PokemonImage,
     PokemonOptions,
   },
-  beforeMount() {
-    getPokemonsOptions()
-  }
+  data() {
+    return {
+      pokemonArr: [],
+      pokemon: null,
+      showPokemon: false,
+      showAnswer: false,
+      msg: "",
+    };
+  },
+  methods: {
+    async mixPokemonArr() {
+      this.pokemonArr = await getPokemonsOptions();
+      const rndInt = Math.floor(Math.random() * 4);
+      this.pokemon = this.pokemonArr[rndInt];
+    },
+    checkAnswer(selectedId, name) {
+      this.showPokemon = true;
+      this.showAnswer = true;
+
+      if (selectedId == this.pokemon.id) {
+        this.msg = `Correcto ${name}`;
+      } else {
+        this.msg = `Uppps, era ${this.pokemon.name}`;
+      }
+    },
+    newGame() {
+      this.showPokemon = false;
+      this.showAnswer = false;
+      this.pokemonArr = [];
+      this.pokemon = null;
+      this.mixPokemonArr()
+    },
+  },
+  mounted() {
+    this.mixPokemonArr();
+  },
 };
 </script>
-
+F
 <style>
 </style>
