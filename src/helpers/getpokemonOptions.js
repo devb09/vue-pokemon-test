a4c1aa3b91dbf902e2463a56cec4fffb5564f008
brn091 <brn091dev@gmail.com>
2023-02-13 21:24:07 -0500

    get 4 random pokemons

--- src/helpers/getpokemonOptions.js
+++ src/helpers/getpokemonOptions.js
@@ -1,15 +1,31 @@
+import pokemonApi from "@/service/pokemonApi";
+
 const getPokemons = () => {
   const pokemonsArr = Array.from(Array(650));
   return pokemonsArr.map((_, index) => index + 1);
 };
 
-const getPokemonsOptions = () => {
+const getPokemonsOptions = async () => {
   const mixedPokemons = getPokemons().sort(() => Math.random() - 0.5);
-  getPokemonNames(mixedPokemons.splice(0, 4));
+  const pokemons = await getPokemonNames(mixedPokemons.splice(0, 4));
+  return pokemons;
 };
 
-const getPokemonNames = ([a, b, c, d] = []) => {
-  console.log(a, b, c, d);
+const getPokemonNames = async ([a, b, c, d] = []) => {
+  const promiseArr = [
+    pokemonApi.get(`/${a}`),
+    pokemonApi.get(`/${b}`),
+    pokemonApi.get(`/${c}`),
+    pokemonApi.get(`/${d}`),
+  ];
+
+  const [p1, p2, p3, p4] = await Promise.all(promiseArr);
+  return [
+    { name: p1.data.name, id: a },
+    { name: p2.data.name, id: b },
+    { name: p3.data.name, id: c },
+    { name: p4.data.name, id: d },
+  ];
 };
 
 export default getPokemonsOptions;
